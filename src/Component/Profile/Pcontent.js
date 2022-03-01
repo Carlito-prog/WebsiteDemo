7c1c2eb651685783e71ae62c55bda2baa847d942
Carlito V <85771184+Carlito-prog@users.noreply.github.com>
2022-02-28 21:35:23 -1000

    Update Pcontent.js

--- src/Component/Profile/Pcontent.js
+++ src/Component/Profile/Pcontent.js
@@ -1,39 +1,39 @@
-import React from 'react'
-import { useRef } from 'react'
-import { store } from '../Store/store'
-import { useSelector } from 'react-redux'
+// import React from 'react'
+// import { useRef } from 'react'
+// import { store } from '../Store/store'
+// import { useSelector } from 'react-redux'
 
 
-const Pcontent = () => {
+// const Pcontent = () => {
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        store.dispatch({type: "add", post: pRef.current.value })
-        pRef.current.value = ""
-    }
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         store.dispatch({type: "add", post: pRef.current.value })
+//         pRef.current.value = ""
+//     }
 
-     const pRef = useRef() 
+//      const pRef = useRef() 
       
-     const post = useSelector((state) => state.post)
+//      const post = useSelector((state) => state.post)
       
-    console.log(post)
+//     console.log(post)
 
-    return (
-       <main className='content-wrp'>
-           <section className='pform-wrp'>
-               <form className='pform'>
-                   <label className='plabel'>
-                        Tweet: 
-                   </label>
-                   <div className='input-wrp'>
-                      <input type="text"  ref={pRef}/>
-                   </div>
-                   <button onSubmit={handleSubmit}> Add Comment </button>
-               </form>
-           </section>
-           <section className='display-post'>
+//     return (
+//        <main className='content-wrp'>
+//            <section className='pform-wrp'>
+//                <form className='pform'>
+//                    <label className='plabel'>
+//                         Tweet: 
+//                    </label>
+//                    <div className='input-wrp'>
+//                       <input type="text"  ref={pRef}/>
+//                    </div>
+//                    <button onSubmit={handleSubmit}> Add Comment </button>
+//                </form>
+//            </section>
+//            <section className='display-post'>
 
-           </section>
+//            </section>
 //            <section>
 //                     {post.map( (sPost) => {
 //                       <>
@@ -47,8 +47,7 @@ const Pcontent = () => {
 //                     </>
 //                     })}
 //            </section>
-       </main>
-    )
-}
+//        </main>
+// )}
 
-export default Pcontent
+// export default Pcontent
